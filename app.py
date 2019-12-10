09823799925fa092ba98c6b4cfcb83039d777b3e
aaaaa <annie4093278@gmail.com>
2019-12-10 15:49:43 +0800

    test

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 
 machine = TocMachine(
-    states=["user", "state1", "state2", "state3"],
+    states=["user", "state1", "state2"],
     transitions=[
         {
             "trigger": "advance",
@@ -28,13 +28,13 @@ machine = TocMachine(
             "dest": "state2",
             "conditions": "is_going_to_state2",
         },
-		{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state3",
-            "conditions": "is_going_to_state3",
-        },
-        {"trigger": "go_back", "source": ["state1", "state2", "state3"], "dest": "user"},
+#		{
+#            "trigger": "advance",
+#            "source": "user",
+#            "dest": "state3",
+#            "conditions": "is_going_to_state3",
+#        },
+        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
     ],
     initial="user",
     auto_transitions=False,
